0699c904977f93880d492c84705026253d25efb3
Pablo Carrasco Perez <pcarper1@upv.es>
2026-04-14 10:32:55 +0200

    ADD: incono de la empresa en el encabezado de la web

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -7,6 +7,7 @@ import BandaView from './pages/bandas/BandaView'
 import BandaConfigView from './pages/bandas/configuracion/BandaConfigView'
 import PerfilLConfigView from './pages/bandas/configuracion/PerfilLConfigView'
 import PerfilTConfigView from './pages/bandas/configuracion/PerfilTConfigView'
+import OndasConfigView from './pages/bandas/configuracion/OndasConfigView'
 import ResumenView from './pages/bandas/ResumenView'
 
 import PedidosView from './pages/pedidos/PedidosView'
@@ -23,6 +24,7 @@ function App() {
           <Route path="/banda/configurar/banda" element={<BandaConfigView />} />
           <Route path="/banda/configurar/perfil-longitudinal" element={<PerfilLConfigView />} />
           <Route path="/banda/configurar/perfil-transversal" element={<PerfilTConfigView />} />
+          <Route path="/banda/configurar/ondas" element={<OndasConfigView />} />
           <Route path="/resumen" element={<ResumenView />}/>
           <Route path="/pedidos" element={<PedidosView />}/>
           <Route path="/importer" element={<ImporterView />}/>
