b10c781c0c433b1446540e8080dae96474f1f733
Pablo Carrasco Perez <pcarper1@upv.es>
2026-09-11 10:20:34 +0200

    EDIT: PerfilL inferior aparecerá primero en la línea

--- backend/app/services/isbue_service.py
+++ backend/app/services/isbue_service.py
@@ -95,7 +95,6 @@ class IsbueService:
             or perfil_l_state.get("inferior", {}).get("color")
         )
 
-        # Revisar estos get
         acotacion_superior = perfil_l_state.get("superior", {}).get("acotado")
         acotacion_inferior = perfil_l_state.get("inferior", {}).get("acotado")
 
@@ -136,6 +135,86 @@ class IsbueService:
         #
         # PERFILES LONGITUDINALES
         #
+
+        if resultado.get("codigo_perfil_inferior"):
+
+            n_inf = resultado.get('n_perfiles_inferior')
+
+            # ACOTACION 1 (CENTRO / CENTRO)
+
+            if acotacion_inferior == 3:
+                observaciones.append(
+                    f"CON {n_inf} PERFILES LONGITUDINALES "
+                    f"TIPO [{tipo_perfil_inferior} "
+                    f"{color_perfil_inferior}] "
+                    f"COLOCADOS A {distancia_entre_centros_I} mm ENTRE CENTROS DE PERFIL "
+                    f"POR LA PARTE INFERIOR"
+                )
+
+            # ACOTACIÓN 2 (CANTO CENTRO-BANDA / CANTO PERFIL)
+
+            if acotacion_inferior == 2:
+                observaciones.append(
+                    f"CON {n_inf} PERFILES LONGITUDINALES "
+                    f"TIPO [{tipo_perfil_inferior} "
+                    f"{color_perfil_inferior}] "
+                    f"COLOCADOS A {distancia_borde_centro_I} mm CANTO CINTA CENTRO PERFIL " # ?
+                    f"POR LA PARTE INFERIOR"
+                )
+
+            # ACOTACIÓN 3 (CANTO BANDA / CANTO PERFIL)
+
+            if acotacion_inferior == 1:
+                observaciones.append(
+                    f"CON {n_inf} PERFILES LONGITUDINALES "
+                    f"TIPO [{tipo_perfil_inferior} "
+                    f"{color_perfil_inferior}] "
+                    f"COLOCADOS A {distancia_borde_banda_I} mm CANTO BANDA CANTO PERFIL "
+                    f"POR LA PARTE INFERIOR"
+                )
+
+            # ACOTACIÓN 4 (INTERIOR / INTERIOR)
+
+            if acotacion_inferior == 4:
+                observaciones.append(
+                    f"CON {n_inf} PERFILES LONGITUDINALES "
+                    f"TIPO [{tipo_perfil_inferior} "
+                    f"{color_perfil_inferior}] "
+                    f"COLOCADOS A {distancia_entre_bandas_I} mm DE INTERIOR A INTERIOR DE PERFIL "
+                    f"POR LA PARTE INFERIOR"
+                )
+
+            # SEGÚN PLANO
+
+            if acotacion_inferior == 5:
+                observaciones.append(
+                    f"CON {n_inf} PERFILES LONGITUDINALES "
+                    f"TIPO [{tipo_perfil_inferior} "
+                    f"{color_perfil_inferior}] "
+                    f"COLOCADOS SEGÚN PLANO "
+                    f"POR LA PARTE INFERIOR"
+                )
+
+            # EXTREMOS
+
+            if acotacion_inferior == 6:
+                observaciones.append(
+                    f"CON {n_inf} PERFILES LONGITUDINALES "
+                    f"TIPO [{tipo_perfil_inferior} "
+                    f"{color_perfil_inferior}] "
+                    f"COLOCADOS EN LOS EXTREMOS DE LA BANDA "
+                    f"POR LA PARTE INFERIOR"
+                )
+
+            # falta ver lo de si es centrado
+            if n_inf == 1:
+                observaciones.append(
+                    f"CON 1 PERFIL LONGITUDINAL "
+                    f"TIPO [{tipo_perfil_inferior} "
+                    f"{color_perfil_inferior}] "
+                    f"COLOCADO EN {resultado.get('distancia_margen_inferior')} mm "
+                    f"DEL EXTREMO POR LA PARTE INFERIOR"
+                )
 
         if resultado.get("codigo_perfil_superior"):
 
@@ -215,86 +294,6 @@ class IsbueService:
                     f"{color_perfil_superior} "
                     f"COLOCADO EN {resultado.get('distancia_margen_superior')} mm "
                     f"DEL EXTREMO POR LA PARTE SUPERIOR"
-                )
-
-        if resultado.get("codigo_perfil_inferior"):
-
-            n_inf = resultado.get('n_perfiles_inferior')
-
-            # ACOTACION 1 (CENTRO / CENTRO)
-
-            if acotacion_inferior == 3:
-                observaciones.append(
-                    f"CON {n_sup} PERFILES LONGITUDINALES "
-                    f"TIPO [{tipo_perfil_inferior} "
-                    f"{color_perfil_inferior}] "
-                    f"COLOCADOS A {distancia_entre_centros_I} mm ENTRE CENTROS DE PERFIL "
-                    f"POR LA PARTE INFERIOR"
-                )
-
-            # ACOTACIÓN 2 (CANTO CENTRO-BANDA / CANTO PERFIL)
-
-            if acotacion_inferior == 2:
-                observaciones.append(
-                    f"CON {n_sup} PERFILES LONGITUDINALES "
-                    f"TIPO [{tipo_perfil_inferior} "
-                    f"{color_perfil_inferior}] "
-                    f"COLOCADOS A {distancia_borde_centro_I} mm CANTO CINTA CENTRO PERFIL " # ?
-                    f"POR LA PARTE INFERIOR"
-                )
-
-            # ACOTACIÓN 3 (CANTO BANDA / CANTO PERFIL)
-
-            if acotacion_inferior == 1:
-                observaciones.append(
-                    f"CON {n_sup} PERFILES LONGITUDINALES "
-                    f"TIPO [{tipo_perfil_inferior} "
-                    f"{color_perfil_inferior}] "
-                    f"COLOCADOS A {distancia_borde_banda_I} mm CANTO BANDA CANTO PERFIL "
-                    f"POR LA PARTE INFERIOR"
-                )
-
-            # ACOTACIÓN 4 (INTERIOR / INTERIOR)
-
-            if acotacion_inferior == 4:
-                observaciones.append(
-                    f"CON {n_sup} PERFILES LONGITUDINALES "
-                    f"TIPO [{tipo_perfil_inferior} "
-                    f"{color_perfil_inferior}] "
-                    f"COLOCADOS A {distancia_entre_bandas_I} mm DE INTERIOR A INTERIOR DE PERFIL "
-                    f"POR LA PARTE INFERIOR"
-                )
-
-            # SEGÚN PLANO
-
-            if acotacion_inferior == 5:
-                observaciones.append(
-                    f"CON {n_sup} PERFILES LONGITUDINALES "
-                    f"TIPO [{tipo_perfil_inferior} "
-                    f"{color_perfil_inferior}] "
-                    f"COLOCADOS SEGÚN PLANO "
-                    f"POR LA PARTE INFERIOR"
-                )
-
-            # EXTREMOS
-
-            if acotacion_inferior == 6:
-                observaciones.append(
-                    f"CON {n_sup} PERFILES LONGITUDINALES "
-                    f"TIPO [{tipo_perfil_inferior} "
-                    f"{color_perfil_inferior}] "
-                    f"COLOCADOS EN LOS EXTREMOS DE LA BANDA "
-                    f"POR LA PARTE INFERIOR"
-                )
-
-            # falta ver lo de si es centrado
-            if n_inf == 1:
-                observaciones.append(
-                    f"CON 1 PERFIL LONGITUDINAL "
-                    f"TIPO [{tipo_perfil_inferior} "
-                    f"{color_perfil_inferior}] "
-                    f"COLOCADO EN {resultado.get('distancia_margen_inferior')} mm "
-                    f"DEL EXTREMO POR LA PARTE INFERIOR"
                 )
 
         #
